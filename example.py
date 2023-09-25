37b4b9208b8c4c19f8799dd82a487103d65998e4
minYong <hucharles@foxmail.com>
2023-09-25 23:22:03 +0800

    feat network

--- example.py
+++ example.py
@@ -1,14 +1,39 @@
-import numpy as np  
-  
-# 读取数据  
-data = np.load('frame_000.npy')  
-  
-# 将数据分为特征和标签  
-X = data[:, :-1] # 假设最后一列是标签  
-y = data[:, -1] # 假设最后一列是标签  
-  
-# 打印前5个样本  
-print("前5个样本的特征：")  
-print(X[:5])  
-print("前5个样本的标签：")  
-print(y[:5])
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# 建立深度学习模型
+def build_model(input_shape):
+    model = models.Sequential()
+    # 添加卷积层
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    # 添加全连接层
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(1, activation='linear'))  # 输出层，用于预测降水量
+
+    # 编译模型
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+    
+    return model
+
+# 准备训练数据和标签
+# X_train是输入数据，包括ZH、ZDR等观测变量，形状为 (样本数, 时间步长, 高度, 宽度, 通道数)
+# y_train是输出数据，即降水量，形状为 (样本数,)
+
+# 创建模型
+input_shape = (10, height, width, num_channels)  # 根据实际数据维度调整
+model = build_model(input_shape)
+
+# 训练模型
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+# 使用模型进行预测
+y_pred = model.predict(X_test)
+
+# 评估模型性能，例如计算均方根误差(RMSE)等
+
